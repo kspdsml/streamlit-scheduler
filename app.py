0795bc58db6f81804052675bfc45bc146cc8e75c
Kishan Patel <kspdsml@gmail.com>
2023-06-07 00:20:45 -0400

    added pets

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+# Initialize connection
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query
+df = conn.query('SELECT * from pets', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f'{row.name} has a :{row.pet}:')
